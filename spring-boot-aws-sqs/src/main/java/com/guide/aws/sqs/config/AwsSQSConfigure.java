07af7651f6f19ea41d409a9ea7fce6883d957db6
jihyeon.jang <jihyeon.jang@sk.com>
2021-01-22 17:52:31 +0900

    [spring-boot-aws-sqs] credential 설정 추가

--- spring-boot-aws-sqs/src/main/java/com/guide/aws/sqs/config/AwsSQSConfigure.java
+++ spring-boot-aws-sqs/src/main/java/com/guide/aws/sqs/config/AwsSQSConfigure.java
@@ -1,13 +1,43 @@
 package com.guide.aws.sqs.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AwsSQSConfigure {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    @Primary
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+    }
+
+    @Bean
+    public AmazonSQS amazonSQSClient() {
+        AmazonSQSClientBuilder builder = AmazonSQSClientBuilder.standard().withCredentials(awsCredentialsProvider());
+        builder.withRegion(region);
+        return builder.build();
+    }
+
     @Bean
     public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSQSAsync) {
         SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
@@ -17,7 +47,6 @@ public class AwsSQSConfigure {
         factory.setTaskExecutor(messageThreadPoolTaskExecutor());   // 메세지를 poll하고 처리하는 handler 함수 설정
         return factory;
     }
-
 
     @Bean
     public ThreadPoolTaskExecutor messageThreadPoolTaskExecutor() {
